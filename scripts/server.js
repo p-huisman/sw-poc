53dd4e61db54fa07ea2b698a19314145998d1569
Peter Huisman <Peter.Huisman@pggm.nl>
2024-02-18 19:52:08 +0100

    removed unused scripts

--- scripts/server.js
+++ scripts/server.js
@@ -1,29 +1,22 @@
 const express = require("express");
 const serveIndex = require("serve-index");
 
-const app = express({strict: false});
+const app = express({ strict: false });
 
-app.use((req, res, next) => { 
+app.use((req, res, next) => {
   res.set("Service-Worker-Allowed", "/");
   res.set("Access-Control-Allow-Origin", "*");
   next();
 });
 
-
-app.use(express.static("./"), serveIndex("./", {icons: true}));
+app.use(express.static("./"), serveIndex("./", { icons: true }));
 
 const fs = require("fs");
-const path = require("path");
 const bodyParser = require("body-parser");
-
+const port = "9000";
 
 app.use(bodyParser.json());
 
-// app.get("/token", (req, res) => {
-//   const data = fs.readFileSync('./scripts/api/token.json', 'utf8');
-//    res.json({token: signToken(data)});
-// });
-
-app.listen("9000", () => {
-  console.log(`Server listening on port 9000`);
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
